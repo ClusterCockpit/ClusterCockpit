6155a58e6ecc072cc6ca549b7eb11bebae1c1326
Jan Eitzinger <jan.eitzinger@fau.de>
2021-04-21 15:05:04 +0200

    Disable graphQL in API platform. Install GraphQLBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
 ];
