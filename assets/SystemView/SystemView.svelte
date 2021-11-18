ccc71aff4099331939b77b9cb2e1a849d5d6137c
Lou Knauer <lou.knauer@gmx.de>
2021-11-18 10:40:56 +0100

    Add RooflinePlot for full cluster

--- assets/SystemView/SystemView.svelte
+++ assets/SystemView/SystemView.svelte
@@ -10,6 +10,7 @@
              Input, InputGroup, InputGroupText } from 'sveltestrap';
     import Resizable from '../Common/Resizable.svelte';
     import TimeseriesPlot from '../Plots/Timeseries.svelte';
+    import RooflinePlot from '../Plots/Roofline.svelte';
 
     export let clusterId;
 
@@ -38,6 +39,17 @@
         to: to.toISOString()
     });
 
+    // TODO: FIXME: Do this calculation server side?
+    // TODO: FIXME: Refetch less often?
+    const rooflineQuery = operationStore(`
+        query($cluster: ID!, $from: Time!, $to: Time!) {
+            nodeMetrics(cluster: $cluster, nodes: null, metrics: ["flops_any", "mem_bw"], from: $from, to: $to) {
+                id,
+                metrics { name, data }
+            }
+        }
+    `, { cluster: clusterId, from: from.toISOString(), to: to.toISOString() });
+
     function updateFilters() {
         if (from == null || to == null)
             return;
@@ -46,7 +58,11 @@
             cluster: clusterId, metrics: [selectedMetric],
             from: from.toISOString(), to: to.toISOString()
         };
-        console.log('query:', ...Object.entries($nodesQuery.variables).flat());
+        $rooflineQuery.variables = {
+            cluster: clusterId,
+            from: from.toISOString(), to: to.toISOString()
+        };
+        // console.log('query:', ...Object.entries($nodesQuery.variables).flat());
     }
 
     function updateExplicitRimeRange(type, event) {
@@ -56,7 +72,45 @@
     }
 
     query(nodesQuery);
+    query(rooflineQuery);
     $: updateFilters(clusterId, selectedMetric, from, to);
+
+    function rooflineTiles() {
+        const rows = 15, cols = 30;
+        let tiles = [];
+        for (let i = 0; i < rows; i++)
+            tiles.push(new Array(cols).fill(0));
+
+        const [minX, maxX, minY, maxY] = [0.01, 1000, 1.,
+            $clustersQuery.clusters.find(c => c.clusterID == clusterId).flopRateSimd];
+        const [lminX, lmaxX, lminY, lmaxY] = [minX, maxX, minY, maxY].map(Math.log10);
+
+        for (let node of $rooflineQuery.data.nodeMetrics) {
+            let flops = node.metrics.find(m => m.name == 'flops_any');
+            let membw = node.metrics.find(m => m.name == 'mem_bw');
+            if (flops == null || membw == null || flops.data.length != membw.data.length)
+                throw new Error("TODO: Error handling...");
+
+            for (let i = 0; i < flops.data.length; i++) {
+                let f = flops.data[i], m = membw.data[i];
+
+                if (m <= 0 || f == null || m == null)
+                    continue;
+                
+                let x = Math.log10(f / m), y = Math.log10(f);
+                if (x < lminX || x > lmaxX || y < lminY || y > lmaxY)
+                    continue;
+
+                x = Math.floor(((x - lminX) / (lmaxX - lminX)) * cols);
+                y = Math.floor(((y - lminY) / (lmaxY - lminY)) * rows);
+                tiles[y][x] += 1;
+            }
+        }
+
+        console.log(tiles);
+
+        return tiles;
+    }
 
 </script>
 
@@ -139,7 +193,25 @@
     {/if}
 </Row>
 
-<br/><br/><br/>
+<br/>
+
+<Row>
+    <Col>
+        {#if $rooflineQuery.fetching}
+            <Spinner secondary/>
+        {:else if $rooflineQuery.error}
+            <Card body color="danger" class="mb-3">
+                <h2>Error: {$rooflineQuery.error.message}</h2>
+            </Card>
+        {:else if !$clustersQuery.fetching}
+            <Resizable let:width>
+                <RooflinePlot width={width}
+                    height={300} cluster={$clustersQuery.clusters.find(c => c.clusterID == clusterId)}
+                    tiles={rooflineTiles($rooflineQuery)} />
+            </Resizable>
+        {/if}
+    </Col>
+</Row>
 
 <Row>
     <Col>
@@ -174,12 +246,14 @@
                             {#if node && node.data}
                                 <span class="plot-title">{node.id}</span>
                                 <Resizable let:width>
+                                    {#key node}
                                     <TimeseriesPlot
                                         metric={node.metric}
                                         clusterId={clusterId}
                                         data={node.data}
                                         height={200}
                                         width={width} />
+                                    {/key}
                                 </Resizable>
                             {:else if node}
                                 <span class="plot-title">{node.id}</span>
